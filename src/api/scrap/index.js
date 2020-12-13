6916864d7c045b9081955edce3ce1bd4ce763b56
cafe <unptitkawa@gmail.com>
2020-12-13 22:03:32 +0100

    api server url

--- src/api/scrap/index.js
+++ src/api/scrap/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import SERVER from '../server';
 
 export async function get_smash_player() {
 	try {
-		const res = await axios.get('http://localhost:3001/api/player/all');
+		const res = await axios.get(`${SERVER}/api/player/all`);
 		return res.data;
 	} catch (e) {
 		console.log(e);
